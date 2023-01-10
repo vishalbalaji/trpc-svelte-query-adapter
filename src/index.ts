16850d0cc1821d48c045af479b7aae3dbbbcdcb7
Vishal Balaji D <vishalbalaji@arch>
2023-01-10 22:51:04 +0530

    chore: add type stubs for all required implementations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const hello = () => console.log('HELLLOOO');
+import type { createTRPCClient, TRPCClientInit } from "trpc-sveltekit";
+import type { AnyRouter } from "@trpc/server";
+import type { Router } from "svelte-app/types";
+
+export const svelteQueryWrapper = (
+	trpc: (init?: TRPCClientInit) => ReturnType<typeof createTRPCClient<Router>>
+) => {
+	return (init?: TRPCClientInit) => {
+		const client = trpc(init)
+		type Client = typeof client;
+
+		type AddQueryPropTypes<T> = { [K in keyof T]:
+			T[K] extends { query: any } ? T[K] & { useQuery: () => unknown, useInfiniteQuery: () => unknown } :
+			T[K] extends { mutate: any } ? T[K] & { useMutation: () => unknown }
+			: AddQueryPropTypes<T[K]> } & {}
+
+		type NewClient = AddQueryPropTypes<Client>
+		let test = {} as NewClient & { useContext: () => unknown, useQueries: () => unknown };
+
+		return test
+	}
+}
+
