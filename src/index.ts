dfb67231ead9ea7c1afefc4a603f5f8f77eaa365
Vishal Balaji D <vishalbalaji@arch>
2023-01-11 18:56:57 +0530

    chore: some cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import type { createTRPCClient, TRPCClientInit } from "trpc-sveltekit";
 import type { AnyRouter } from "@trpc/server";
 import type { Router } from "svelte-app/types";
 
-export const svelteQueryWrapper = (
-	trpc: (init?: TRPCClientInit) => ReturnType<typeof createTRPCClient<Router>>
-) => {
-	return (init?: TRPCClientInit) => {
-		const client = trpc(init)
-		type Client = typeof client;
+type AddQueryPropTypes<T> = { [K in keyof T]:
+	T[K] extends { query: any } ? T[K] & { useQuery: () => T[K]["query"], useInfiniteQuery: () => T[K]["query"] } :
+	T[K] extends { mutate: any } ? T[K] & { useMutation: () => T[K]["mutate"] }
+	: AddQueryPropTypes<T[K]> } & {};
 
-		type AddQueryPropTypes<T> = { [K in keyof T]:
-			T[K] extends { query: any } ? T[K] & { useQuery: () => unknown, useInfiniteQuery: () => unknown } :
-			T[K] extends { mutate: any } ? T[K] & { useMutation: () => unknown }
-			: AddQueryPropTypes<T[K]> } & {}
+
+export function svelteQueryWrapper(
+	trpc: (init?: TRPCClientInit) => ReturnType<typeof createTRPCClient<Router>>
+) {
+	return (init?: TRPCClientInit) => {
+		const client = trpc(init);
+		type Client = typeof client;
 
 		type NewClient = AddQueryPropTypes<Client>
 		let test = {} as NewClient & { useContext: () => unknown, useQueries: () => unknown };
 
-		return test
-	}
-}
-
+		return test;
+	};
+};
