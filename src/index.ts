56482ba28876b8b2d2fd6215d4869675e49b4ca2
Vishal Balaji D <vishalbalaji@arch>
2023-01-13 23:07:10 +0530

    feat: implement `useQuery`, `useInfiniteQuery` and `useMutation`

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,80 @@
-import type { createTRPCClient, TRPCClientInit } from 'trpc-sveltekit';
-import type { AnyRouter } from '@trpc/server';
-import type { Router } from '../../../apps/svelte-app/types';
 import DeepProxy from 'proxy-deep';
 
-type Procedure = 'query' | 'mutate' | 'subscribe' | 'useQuery' | 'useInfiniteQuery' | 'useMutation'
+// import type { AnyRouter } from '@trpc/server';
+import type { Router as TRouter } from '../../../apps/svelte-app/types';
+import type { createTRPCClient, TRPCClientInit } from 'trpc-sveltekit';
+import {
+	createQuery,
+	createMutation,
+	createInfiniteQuery,
+	type CreateQueryOptions,
+	type CreateMutationOptions,
+	type CreateInfiniteQueryOptions,
+} from '@tanstack/svelte-query';
+import type { TRPCClientError } from '@trpc/client';
 
-type AddQueryPropTypes<T> = { [K in keyof T]:
-	T[K] extends { query: any } ? T[K] & { useQuery: T[K]['query'], useInfiniteQuery: T[K]['query'] } :
-	T[K] extends { mutate: any } ? T[K] & { useMutation: T[K]['mutate'] }
-	: AddQueryPropTypes<T[K]> } & {};
+enum ProcedureNames {
+	query = 'useQuery',
+	infiniteQuery = 'useInfiniteQuery',
+	mutate = 'useMutation',
+	subscribe = 'useSubscription',
+}
+
+type UseQueryProcedure<TInput, TOutput, TError> = {
+	[ProcedureNames.query]: (input: TInput, opts?: CreateQueryOptions<TOutput, TError>)
+		=> ReturnType<typeof createQuery<Awaited<TOutput>, TError>>
+}
+type UseInfiniteQueryProcedure<TInput, TOutput, TError> = TInput extends { cursor?: any }
+	? {
+		[ProcedureNames.infiniteQuery]: (input: Omit<TInput, 'cursor'>, opts?: CreateInfiniteQueryOptions<TOutput, TError>)
+			=> ReturnType<typeof createInfiniteQuery<Awaited<TOutput>, TError>>
+	}
+	: {}
+type QueryProcedures<TInput, TOutput, TError> = UseQueryProcedure<TInput, TOutput, TError> & UseInfiniteQueryProcedure<TInput, TOutput, TError>
+
+type UseMutationProcedure<TInput, TOutput, TError> = {
+	[ProcedureNames.mutate]: (input: TInput, opts?: CreateMutationOptions<TInput, TError>)
+		=> ReturnType<typeof createMutation<Awaited<TOutput>, TError>>
+}
+
+type UseSubscriptionProcedure<TInput, TOutput, TError> = never
+
+type AddQueryPropTypes<T, TError> = { [K in keyof T]:
+	T[K] extends { query: any } ? QueryProcedures<Parameters<T[K]['query']>[0], ReturnType<T[K]['query']>, TError>
+	: T[K] extends { mutate: any } ? UseMutationProcedure<Parameters<T[K]['mutate']>[0], ReturnType<T[K]['mutate']>, TError>
+	: T[K] extends { subscribe: any } ? UseSubscriptionProcedure<Parameters<T[K]['subscribe']>[0], ReturnType<T[K]['subscribe']>, TError>
+	: AddQueryPropTypes<T[K], TError>
+};
 
 export function svelteQueryWrapper(
-	trpc: (init?: TRPCClientInit) => ReturnType<typeof createTRPCClient<Router>>
+	trpc: (init?: TRPCClientInit) => ReturnType<typeof createTRPCClient<TRouter>>
 ) {
+	type RouterError = TRPCClientError<TRouter>
+
 	return (init?: TRPCClientInit) => {
 		const client = trpc(init);
 		type Client = typeof client;
 
-		type NewClient = AddQueryPropTypes<Client> & { useContext: () => unknown, useQueries: () => unknown }
-		let test = new DeepProxy({} as NewClient, {
+		type ClientWithQuery = AddQueryPropTypes<Client, RouterError> & { useContext(): unknown, useQueries: unknown }
+		return new DeepProxy({} as ClientWithQuery, {
 			get() {
-				return this.nest(function() { })
+				return this.nest(() => {})
 			},
 
 			apply(_target, _thisArg, argList) {
-				const procedure = this.path[this.path.length - 1] as Procedure
-				console.log({ path: this.path, procedure, argList })
+				const procedure = this.path.pop()
+				const target = [...this.path].reduce((client, value) => client[value], client as Record<string, any>)
 
-				if (procedure === 'query' || procedure === 'mutate' || procedure === 'subscribe') {
-					const procFunc = [...this.path].reduce(
-						(client, cur) => (client as unknown as Record<string, any>)[cur],
-						client
-					) as unknown as () => unknown
-
-					return procFunc(...argList as [])
+				if (procedure === ProcedureNames.query) {
+					return createQuery(this.path, () => (target["query"] as (args: any[]) => any)(argList[0]), argList[1])
+				} else if (procedure === ProcedureNames.infiniteQuery) {
+					return createInfiniteQuery(this.path, () => (target["query"] as (args: any[]) => any)(argList[0]), argList[1])
+				} else if (procedure === ProcedureNames.mutate) {
+					return createMutation(this.path, () => (target["mutate"] as (args: any[]) => any)(argList[0]), argList[1])
 				}
 
-				return 'foo';
+				return target[procedure as string]();
 			}
 		});
-
-		return test;
 	};
 };
