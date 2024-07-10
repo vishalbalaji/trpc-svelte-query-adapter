8fc3a46572421c7efffa8da457d0a50d63a97620
vishalbalaji <vishalbalaji@gmail.com>
2024-07-10 22:29:07 +0530

    chore: add zod to test app [skip ci]

--- @app/src/lib/trpc/router.ts
+++ @app/src/lib/trpc/router.ts
@@ -1,13 +1,16 @@
 import type { Context } from '$lib/trpc/context';
 import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
 
 export const t = initTRPC.context<Context>().create();
 
 export const router = t.router({
-	greeting: t.procedure.query(async () => {
-		await new Promise((r) => setTimeout(r, 500));
-		return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
-	})
+	greeting: t.procedure
+		.input(z.string().optional())
+		.query(async ({ input }) => {
+			await new Promise((r) => setTimeout(r, 500));
+			return `Hello ${input ?? 'tRPC'} v10 @ ${new Date().toLocaleTimeString()}`;
+		})
 });
 
 export const createCaller = t.createCallerFactory(router);
