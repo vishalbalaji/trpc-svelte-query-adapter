f3ccb316c9a2d5b1c557d137bf1bbaa83c13347c
vishalbalaji <vishalbalaji@gmail.com>
2024-07-11 23:33:53 +0530

    docs: revert example app [skip ci]

--- @app/src/routes/+page.svelte
+++ @app/src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <script lang="ts">
+	export let data;
+	const queries = data.queries();
 </script>
+
+<pre>
+{JSON.stringify($queries, null, 2)}
+</pre>
+
---
+++ @app/src/routes/+page.ts
@@ -0,0 +1,15 @@
+import { trpc } from '$lib/trpc/client';
+
+export async function load(event) {
+	const { queryClient } = await event.parent();
+	const api = trpc(event, queryClient);
+
+	return {
+		queries: await api.createServerQueries((t) => [t.greeting('foo'), t.greeting('bar')], {
+			combine: (results) => ({
+				data: results.map(({ data }) => data),
+				pending: results.some(({ isPending }) => isPending),
+			}),
+		}),
+	};
+}
